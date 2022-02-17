4cd13d32996c5fdb9a99ea0ed7e62934f966e260
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2022-02-18 03:16:08 +0900

    Add files via upload

--- Hello/Hello/MainApp.cs
+++ Hello/Hello/MainApp.cs
@@ -2,11 +2,18 @@
 
 namespace Hello
 {
-    class Program
+    
+    class MainApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+            Console.WriteLine("Hello, {0}", args[0]);
         }
     }
 }
